a416bf862e8d4a403e583496c965d657e30a5ee7
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-15 12:43:30 +0500

    Second-Updation

---
+++ script.js
@@ -0,0 +1,24 @@
+let red = document.getElementById('red');
+let yellow = document.getElementById('yellow');
+let green = document.getElementById('green');
+let StartAuto = document.getElementById('startAuto');
+
+let counter;
+let currentLight = 'red';
+let timeLeft = 10;
+
+
+function turnOffLight () {
+    red.style.backgroundColor = 'grey'
+    yellow.style.backgroundColor = 'grey'
+    green.style.backgroundColor = 'grey'
+}
+
+function setRed () {
+    turnOffLight();
+    red.backgroundColor='red';
+    currentLight = 'red';
+    timeLeft = 10;
+    UpdateTimer();
+}
+
